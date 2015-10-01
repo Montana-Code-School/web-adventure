b5439a535ce7b747f81b1c446ac504fe9567cd14
Zachary Warren <zach@Zacharys-MacBook-Pro.local>
2015-10-01 10:09:27 -0600

    added adventure.js

---
+++ adventure.js
@@ -0,0 +1,14 @@
+//var yCoord = 0;
+//var xCoord = 0;
+//
+//// Initializing the corrdinates of the target cell 
+//var endX = 2;
+//var endY = 2;
+
+var adventure = {
+    yCoord: 0,
+    xCoord: 0,
+    endX: 0,
+    endY: 0
+};
+
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//var adventure = require('./adventure');
+
 var yCoord = 0;
 var xCoord = 0;
 
@@ -7,22 +9,22 @@ var endY = 2;
 
 //Adding a function to replace the winning cell with a picture of dead Abe Simpson
 var killAbe = function () {
-	document.getElementById(endY.toString() + endX.toString()).innerHTML = "<img src=\"dead.jpg\"></img>";
+    document.getElementById(endY.toString() + endX.toString()).innerHTML = "<img src=\"dead.jpg\"></img>";
 };
 
 //Adding a function to place an image at the target cell
 var target = function(){
-	document.getElementById(endY.toString() + endX.toString()).innerHTML = "<img src=\"retire.png\"></img>";
+    document.getElementById(endY.toString() + endX.toString()).innerHTML = "<img src=\"retire.png\"></img>";
 };
     
 var movAbe = function(){
-	document.getElementById(yCoord.toString() + xCoord.toString()).innerHTML = "<img src=\"small_abe.png\"></img>";
+    document.getElementById(yCoord.toString() + xCoord.toString()).innerHTML = "<img src=\"small_abe.png\"></img>";
 };
 
 //refactoring hideAbe
 
 var hideAbe = function (xChange, yChange) {
-	document.getElementById((yCoord + yChange).toString() + (xCoord + xChange).toString()).innerHTML = "";
+    document.getElementById((yCoord + yChange).toString() + (xCoord + xChange).toString()).innerHTML = "";
 }
 
 var holdon = new Audio('holdon.wav');
@@ -34,16 +36,16 @@ movAbe();
 target();
 
 function mov(xDel, yDel, x, y, bound) {
-	
-	if(xCoord === (endX + xDel) && yCoord === (endY + yDel)) {
+    
+    if(xCoord === (endX + xDel) && yCoord === (endY + yDel)) {
         xCoord += x;
         yCoord += y;
-        hideAbe(xDel, yDel);	
+        hideAbe(xDel, yDel);    
         killAbe();
         dead.play();
         alert("Whuuthaa!!??");
-	}
-	else if (yCoord === bound && yDel != 0) {
+    }
+    else if (yCoord === bound && yDel != 0) {
         holdon.play();
         alert("D'oh!!!");
     }
@@ -54,22 +56,22 @@ function mov(xDel, yDel, x, y, bound) {
         else {
         xCoord += x;
         yCoord += y;
-		hideAbe(xDel, yDel);
-		movAbe();
-	};
+        hideAbe(xDel, yDel);
+        movAbe();
+    };
 }
 
 function movDown() {
-	mov(0, -1, 0, 1, 2);
+    mov(0, -1, 0, 1, 2);
 }; 
 function movUp() {
-	mov(0, 1, 0, -1, 0);
+    mov(0, 1, 0, -1, 0);
 }; 
 function movLeft() {
-	mov(1, 0, -1, 0, 0);
+    mov(1, 0, -1, 0, 0);
 }; 
 function movRight() {
-	mov(-1, 0, 1, 0, 2);
+    mov(-1, 0, 1, 0, 2);
 };
 
 function reset() {
@@ -79,77 +81,5 @@ function reset() {
     movAbe();
     target();
 }
-//function movDown() {
-//	
-//	
-//	if(xCoord === endX && yCoord === (endY -1)) {
-//        yCoord++;
-//        hideAbe(0, -1);	
-//        killAbe();
-//        alert("Whuuthaa!!??");
-//	}
-//	else if (yCoord === 2) {
-//        alert("D'oh!!!");
-//    }
-//        else {
-//        yCoord++;
-//		hideAbe(0, -1);
-//		movAbe();
-//	};
-//}
-//
-//function movUp() {
-//	
-//	if(xCoord === endX && yCoord === (endY +1)) {
-//        yCoord--;
-//        hideAbe(0,1);
-//        killAbe();
-//	   alert("WhuuThe!!??");
-//    }
-//	else if (yCoord === 0){
-//		alert("D'oh!!!");
-//	}
-//	else {
-//		yCoord--;
-//		hideAbe(0, 1);
-//		movAbe();
-//	};
-//}
-//
-//function movLeft() {
-//	
-//	if(xCoord === (endX + 1) && yCoord === endY) {
-//       xCoord--;
-//       hideAbe(1,0);
-//       killAbe();
-//	   alert("WhuuThe!!??");
-//    }
-//	else if (xCoord === 0){
-//		alert("D'oh!!!");
-//	}
-//	else {
-//		xCoord--;
-//		hideAbe(1,0);
-//		movAbe();
-//	};
-//}
-//
-//function movRight() {
-//	
-//	if(xCoord === (endX - 1) && yCoord === endY) {
-//        xCoord++;
-//        hideAbe(-1,0);
-//        killAbe();
-//	   alert("WhuuThe!!??");
-//    }
-//	else if (xCoord === 2){
-//		alert("Don't have cow, man!");
-//	}
-//	else {
-//		xCoord++;
-//        hideAbe(-1,0);
-//		movAbe();
-//	};
-//}
 
 
